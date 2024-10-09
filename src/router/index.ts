885f89640ab0448ddd19b76985c31b586d3de31e
qinhj <qhj@systech.com.cn>
2024-10-09 15:37:07 +0800

    feat: 搭建二级路由视图验证页面卸载效果

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/home',
+      component: () => import('@/views/home/HomeView.vue'),
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          component: () => import('@/views/home/components/SubHomeView.vue')
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: () => import('@/views/home/components/AboutView.vue')
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login/LoginView.vue')
     }
   ]
 })
