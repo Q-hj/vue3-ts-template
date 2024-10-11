915c0a969251c71a75c5808111d16f7421521db1
qinhj <qhj@systech.com.cn>
2024-10-11 14:43:12 +0800

    响应式测试

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const obj = ref({
+  x: 0,
+  y: 0,
+});
+
+setTimeout(() => {
+  // 可以触发响应式
+  Object.assign(obj.value, { x: 1, y: 2 });
+}, 2000);
+</script>
+
 <template>
+  <p>{{ obj }}</p>
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/login">login</RouterLink>
