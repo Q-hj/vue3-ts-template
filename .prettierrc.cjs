6bc41fbfe7a7583328f013323c4825c2f08e5ed8
qinhj <qhj@systech.com.cn>
2024-10-14 15:27:03 +0800

    feat(格式化): 去除函数参数换行的尾随逗号

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,11 +2,11 @@ module.exports = {
   /** 使用单引号而不是双引号，默认 false */
   singleQuote: true,
 
+  /** 是否尾随逗号，默认all */
+  trailingComma: 'es5', //在 ES5 中有效的尾随逗号（对象、数组等）
+
   /** 行尾符号，默认 lf */
   // endOfLine: 'auto',
-
-  /** 是否尾随逗号，默认all */
-  // trailingComma: 'all',
 
   /** 在语句的末尾打印分号，默认 true */
   // semi: false,
